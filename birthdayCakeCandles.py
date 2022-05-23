81ac278562d01a86e006e2c8f2c5dfba1f6f125a
RKaur <32834083+sherryjasal@users.noreply.github.com>
2022-05-23 06:41:01 +0530

    Create birthdayCakeCandles.py

---
+++ birthdayCakeCandles.py
@@ -0,0 +1,11 @@
+##cake will have one candle for each year of their total age. 
+##They will only be able to blow out the tallest of the candles. 
+## Count how many candles are tallest.
+
+def birthdayCakeCandles(candles):
+    count = 0
+    max_candles = max(candles)
+    for i in candles:
+        if i == max_candles:
+            count +=1
+    return count
