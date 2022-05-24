1f058031fb977ff5ec894e4343e6f0c60b6dd920
RKaur <32834083+sherryjasal@users.noreply.github.com>
2022-05-24 17:12:58 +0530

    Create sortArray.py

---
+++ sortArray.py
@@ -0,0 +1,7 @@
+    def sortArr(self, arr, n): 
+       new_arr = list(arr)
+       new_arr.sort()
+       return new_arr
+    
+    
+    ##lessons: list(arr) - otherwise it treats arr as empty so use list(arr)
